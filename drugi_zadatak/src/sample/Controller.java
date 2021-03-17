c4dddbcb27c0ce4b00aae97f0fff46da9b3d8cd7
ealic4 <ealic4@etf.unsa.ba>
2021-03-17 20:29:04 +0100

    Drugi zadatak

---
+++ drugi_zadatak/src/sample/Controller.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+
+
+
+public class Controller {
+    public Label helloWorld;
+    public GridPane pozadina;
+
+    public void funkcija(ActionEvent actionEvent){
+
+        helloWorld.setText("Hello World!");
+        helloWorld.setStyle("-fx-background-color: red");
+        helloWorld.setStyle("-fx-font-family: Arial");
+        helloWorld.setStyle(" -fx-font-style: italic;");
+        helloWorld.setStyle("-fx-font-size: 32");
+
+        pozadina.setStyle("-fx-background-color: red");
+
+    }
+}
